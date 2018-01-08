ce3485f6a1e989688b05d7ee4f140408fed9661a
Jonathan Vargas <jonathanv3232@gmail.com>
2018-01-08 14:00:31 -0500

    adds rainbow stroke

--- main.js
+++ main.js
@@ -1,26 +1,28 @@
 const canvas = document.querySelector('#draw');
 const ctx = canvas.getContext('2d');
+let hue = 0;
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
-ctx.strokeSyle = 'blue';
+ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
 ctx.lineJoin = 'round';
 ctx.lineCap = 'round';
-ctx.lineWidth = '50';
+ctx.lineWidth = '15';
 let isDrawing = false;
 let x = 0;
 let y = 0;
 
 function draw(e) {
-  if (isDrawing) {
-    console.log(e);
-    ctx.beginPath();
-    ctx.moveTo(x, y);
-    ctx.lineTo(e.offsetX, e.offsetY);
-    ctx.stroke();
-    x = e.offsetX;
-    y = e.offsetY;
+  if (!isDrawing) {
+    return;
   }
-  return;
+  ctx.beginPath();
+  ctx.moveTo(x, y);
+  ctx.lineTo(e.offsetX, e.offsetY);
+  ctx.stroke();
+  x = e.offsetX;
+  y = e.offsetY;
+  hue++;
+  ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
 }
 
 canvas.addEventListener('mousedown', e => {
@@ -33,3 +35,4 @@ canvas.addEventListener('mouseup', () => {
   isDrawing = false;
 });
 canvas.addEventListener('mousemove', draw);
+// canvas.addEventListener('mouse');
